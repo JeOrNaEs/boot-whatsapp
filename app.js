a7fcd0c512b6977a709e10a1aed856d6aa86ec14
JeOrNaEs <36236409+JeOrNaEs@users.noreply.github.com>
2023-10-15 11:20:49 -0500

    Tercer Commit

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const fs = require('fs')
 const ora = require('ora')
 const chalk = require('chalk')
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-
 const SESSION_FILE_PATH = './session.json';
 
 let client;
@@ -13,52 +10,17 @@ let sessionData;
  * Esta funcion genera el QRCODE ***
  */
 
-const withSession = () => {
+const qrcode = require('qrcode-terminal');
 
-    const spinner = ora(`Cargando ${chalk.yellow('Validando session con Whatsapp')}`);
-    sessionData = require(SESSION_FILE_PATH);
-    spinner.Start();
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
 
-    client = new Client({
-        session:sessionData
-    })
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
-    client.on('ready', () => {
-        console.log('Client is ready!');
-        spinner.Stop();
-    });
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
 
-    client.on('auth_failure', () => {
-        spinner.Stop();
-        console.log('** Error de autentificacion vuelve a generar el QRCODE (Borra el a)');
-    })
-
-    client.initialize();
-}
-
-const withOutSession = () => {
-
-    console.log('No tenemos session guardada');
-    
-    client = new Client();
-    
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, { small : true });
-    });
-    
-    client.on('authenticated', (session) => {
-        //Guardamos credenciales de sission para usar luego
-        sessionData = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
-            if(err){
-                console.log(err);
-            }
-        });
-    });
-
-    client.initialize();
-
-}
-
-/** */
-(fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
+client.initialize();
